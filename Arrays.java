01daafcc8a95e2847dd9c2412dda6a2907e02e6b
Alessandro Almeida <90009634+ale-almeida@users.noreply.github.com>
2022-03-13 17:59:33 -0300

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// Listar 10 números inteiros com arrays
+		int maximo = 10;
+		int arrays [] = new  int [maximo];
+		for (int i = 0; i < maximo; i++) {
+			arrays [i] =i;
+			System.out.print(arrays[i]+ " ");
+
+		}
+	}
+}
